2db1631541558764b16859af1f6015dd327bf390
AmeerHamzaKhan97 <hamzakn97@gmail.com>
2021-07-31 00:13:56 +0530

    update all files

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [pictures, setPictures] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   const [page, setpage] = useState(pageNumber);
+  const [queries, setQueries] = useState(null);
 
   useEffect(() => {
     const getAllPhotos = async () => {
@@ -34,12 +35,15 @@ function App() {
   window.onscroll = function () {
     
     if (
-      window.scrollY + window.innerHeight >=
-      document.documentElement.scrollHeight
+      window.scrollY /
+      document.documentElement.scrollHeight >=0.95
     )
-    //console.log("working")
+   
     {
+       console.log("working" ,window.scrollY , window.innerHeight,
+      document.documentElement.scrollHeight)
       scrollToFind();
+
     }
   };
 
@@ -52,13 +56,20 @@ function App() {
   function search(event) {
     event.preventDefault();
     console.log("searching...");
+    let tag = searchValue;
+    if(event.target.query){
+      tag = event.target.query
+    }
+    
    
-    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e58db5c37e31c2add7ff47165e528488&safe_search=1&tags=${searchValue}&format=json&nojsoncallback=1`;
+    setQueries(null)
+    let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e58db5c37e31c2add7ff47165e528488&safe_search=1&tags=${tag}&format=json&nojsoncallback=1`;
     fetch(url)
       .then((resp) => resp.json())
       .then((data) => {
         console.log({ data });
         setPictures(data.photos.photo);
+        
       });
     let names = JSON.parse(localStorage.getItem("names"));
 
@@ -74,7 +85,16 @@ function App() {
     }
   }
 
-   
+  //  function getData(){
+  //    const data = (localStorage.getItem('names'))
+  //    console.log(data)
+    
+  //  }
+  const searchQuery = () => {
+    console.log("ff");
+    setSearchValue("query");
+    // search()
+  };
 
   
   
@@ -85,13 +105,40 @@ function App() {
         <div className="app-search-section">
           <div>
             <h2>Search Photos</h2>
-            <form onSubmit={search}>
+            <form
+              onSubmit={search}
+              onBlur={() => {
+                setQueries(null);
+              }}
+            >
               <input
                 type="text"
                 value={searchValue}
-               
+                onFocus={() => {
+                  const data = JSON.parse(localStorage.getItem("names"));
+                  setQueries(data);
+                }}
                 onChange={updateSearchValue}
               ></input>
+              <ul id="query-list">
+                {queries &&
+                  queries.map((query) => (
+                    <li
+                      key={query}
+                      onClick={(e) => {
+                        console.log("ff");
+                        setSearchValue(query);
+                        e.target.query = query;
+                        search(e);
+                      }}
+                    >
+                      {query}
+                    </li>
+                  ))}
+                {/* <li key="clear_btn" onClick={searchQuery}>
+                  <button>JJ</button>
+                </li> */}
+              </ul>
             </form>
           </div>
         </div>
@@ -99,7 +146,9 @@ function App() {
 
       <div className="my-image">
         {pictures.length > 0 &&
-          pictures.map((img,i) => <ImageCard image_src={img} key={img.id+i} />)}
+          pictures.map((img, i) => (
+            <ImageCard image_src={img} key={img.id + i} />
+          ))}
       </div>
     </div>
   );
--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
- import InfiniteScroll from "react-infinite-scroll-component";
+
 import { useState } from 'react';
 
 
@@ -8,15 +8,73 @@ function parseSrc(image){
     return `https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`
 }
 function ImageCard(props) {
-  
+  const [isOpen,setIsOpen] = useState(false)
+  const [model, setModel] = useState(true)
+ 
+  function showImg(){
+      setIsOpen(!isOpen)
+      setModel(!model)
+      console.log("clicked")
+      
+      
+  }
+
+   let a = model ? "normal" : "click";
+
+    // return (
+    //   <Fragment>
+    //     <li>
+    //       <img
+    //         onClick={showImg}
+    //         // className={a
+    //         src={parseSrc(props.image_src)}
+    //         alt={props.image_src}
+    //       />
+    //       {isOpen && (
+    //         <dialog
+    //           className={a}
+    //           style={{ position: "fixed" }}
+    //           open
+    //           onClick={showImg}
+    //         >
+    //           <img
+    //             onClick={showImg}
+    //            className={a}
+    //             src={parseSrc(props.image_src)}
+    //             alt={props.image_src}
+    //           />
+    //         </dialog>
+    //       )}
+    //     </li>
+    //   </Fragment>
+    // );
     return (
-        <Fragment>
-        
-            <li>
-            <img  src={parseSrc(props.image_src)} alt={props.image_src}/>
-            </li>
-        </Fragment>
-    )
+      
+        <li>
+          <img
+            className="small"
+            src={parseSrc(props.image_src)}
+            onClick={showImg}
+            alt={props.image_src}
+          />
+          {isOpen && (
+            <dialog
+              className="dialog"
+              style={{ position: "fixed" }}
+              open
+              onClick={showImg}
+            >
+              <img
+                className="image"
+                src={parseSrc(props.image_src)}
+                onClick={showImg}
+                alt={props.image_src}
+              />
+            </dialog>
+          )}
+        </li>
+      
+    );
 }
 
 export default ImageCard
